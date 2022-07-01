6f338315e1280cbe1ffa5a55bb5afc4114015f50
HAGER <48705580+hager51@users.noreply.github.com>
2022-07-01 16:00:23 +0200

    Add files via upload

---
+++ Lab05/CityDao.java
@@ -0,0 +1,21 @@
+//package Lab5;
+
+//import Lab5.City;
+
+import java.util.List;
+
+public interface CityDao {
+    City findCityById(int id);
+
+    City removeCity(City city);
+
+    City addCity(City city);
+
+    City updateCity(City city);
+
+    List<City> findAllCities();
+
+    List<City> findCitiesByCountryCode(String countryCode);
+
+    List<City> readCitiesFromFile(String fileName);
+}
